25b0e47e3a9215c1133150e6b005ef7c3a62ce86
prachi0513 <vatsprachi10@gmail.com>
2022-06-11 17:27:53 +0530

    js-fun-unction

--- Functions/Functions.js
+++ Functions/Functions.js
@@ -1,6 +1,4 @@
 
-x()
-a()
 
 //function statement
 function x() {
@@ -12,7 +10,39 @@ function x() {
 let a  = function () {
   console.log("a called");
 }
+x()
+a()
 
 //difference between function statement and function expression is the hoisting .
 //we can access the function statement before initialization but we cant access the expression before initialization because
 //it is trated as variable.
+
+ 
+//Anonymous function
+// function (){
+
+// }
+
+//Named function expression
+let y = function xyz() {
+    console.log("named function called");
+}
+
+y()
+
+//Argument and parameter
+
+function q (parameter){
+    console.log("q");
+}
+
+q(Array)
+
+//First clas function  -- > Ability to be used as a values
+// passed to anothe rfunction and return from them is know as fire class function.
+
+
+//first class citizens -- > Same as first class function.
+
+
+//Arrow Function
