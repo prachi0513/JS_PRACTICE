0f8a103b6272470395dc06b78938ef69008d36bf
prachi0513 <vatsprachi10@gmail.com>
2022-06-17 21:12:45 +0530

    new Currying

--- Functions/currying.js
+++ Functions/currying.js
@@ -2,14 +2,13 @@ function multiply(x, y) {
   console.log(x * y);
 }
 
-
 let output = multiply.bind(this,2);
 output(3)
 
 let three = multiply.bind(this,4);
 three(4)
 
-//--by using closuress-/// 
+//--by using closuress-///
 
 function fun(x){
     return function(y){
@@ -17,5 +16,16 @@ function fun(x){
     }
 }
 
-let outputClosuress = fun(3) 
+let outputClosuress = fun(3)
 outputClosuress(4)
+
+// --------------------//
+
+function add(a) {
+  return function s(b) {
+    console.log(a + b); 
+  };
+}
+
+let res = add(3)(5);
+
