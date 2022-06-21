e0e93eacebb7c324fd2dcf8c649cd78c02913aed
prachi0513 <vatsprachi10@gmail.com>
2022-06-21 19:51:11 +0530

    Event propagaton

--- PlayGround/Event.js
+++ PlayGround/Event.js
@@ -1,13 +1,31 @@
 console.log("Starting");
 
-document.querySelector("#granparent").addEventListener("click", function () {
-  console.log("grandparent clicked");
-});
+document.querySelector("#granparent").addEventListener(
+  "click",
+  function () {
+    console.log("grandparent clicked");
+  },
+  true
+);
 
-document.querySelector("#parent").addEventListener("click", function () {
-  console.log("parent clicked");
-});
+document.querySelector("#parent").addEventListener(
+  "click",
+  function () {
+    console.log("parent clicked");
+  },
+  false
+);
 
-document.querySelector("#child").addEventListener("click", function () {
-  console.log("child clicked");
-});
+document.querySelector("#child").addEventListener(
+  "click",
+  function (e) {
+    console.log("child clicked");
+    e.stopPropagation();
+  },
+  false
+);
+
+
+//when we give true argument event capturing will happen .
+//when it is false , which is default behaviour it will act like bubbling .
+//if we want to stop this peopagation we cant ' event.stopPropagation() ' function.
