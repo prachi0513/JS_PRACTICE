cfe04295dc8f5932066c827a87008cb3a1ab6f05
prachi0513 <vatsprachi10@gmail.com>
2022-06-28 23:24:04 +0530

    Anagram up

--- Practice/anagram.js
+++ Practice/anagram.js
@@ -5,6 +5,8 @@ function checkStringsAnagram(a, b) {
        console.log('Invalid Input');
        return
     }
+
+    
     let str1 = a.split('').sort().join('');
     let str2 = b.split('').sort().join('');
     if(str1 === str2){
