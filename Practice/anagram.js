b2d4a33c1152ff6cb5169e9a9ad34ec43f9bd39b
prachi0513 <vatsprachi10@gmail.com>
2022-06-29 15:59:36 +0530

    Anag

--- Practice/anagram.js
+++ Practice/anagram.js
@@ -1,18 +1,17 @@
-function checkStringsAnagram(a, b) {
-    let len1 = a.length;
-    let len2 = b.length;
-    if(len1 !== len2){
-       console.log('Invalid Input');
-       return
-    }
+function checkAnagram(str1, str2) {
+  let len1 = str1.length;
+  let len2 = str2.length;
 
-    
-    let str1 = a.split('').sort().join('');
-    let str2 = b.split('').sort().join('');
-    if(str1 === str2){
-       console.log("True");
-    } else { 
-       console.log("False");
-    }
- }
- checkStringsAnagram("indian","ndiani")
+  if (len1 !== len2) {
+    return false;
+  }
+
+  if (str1.split("").sort().join() === str2.split("").sort().join()) {
+   return true;
+  }else{
+   return false;
+  }
+}
+
+let ans = checkAnagram("indian", "naadni");
+console.log(ans);
